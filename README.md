aa45644b8cf01c5637a2a0aeb6be60e6fc47bed3
Eli Smukler <esmukler@pandora.com>
2021-03-02 12:49:14 -0800

    Add multiple dates/case numbers per county to CSV

--- README.md
+++ README.md
@@ -2,7 +2,13 @@
 Tracking Covid-19 Cases in Counties with NBA Arenas
 
 To run:
-Enter into the terminal: `python -m http.server`
-Navigate to localhost:8000 in your browser.
+- Enter into the terminal: `python -m http.server`
+- Navigate to localhost:8000 in your browser.
 
 ![data viz screenshot](assets/dataVizScreengrab.png)
+
+
+To Dos:
+- Show line graphs for each county over time
+- Update data on pageload?
+- Store old data while adding new data on update
--- parse.py
+++ parse.py
@@ -12,33 +12,38 @@ with requests.Session() as s:
     county_date_data = list(cr)
 
 
-def match(county_date, team_county):
+def match_county(county_date, team_county):
     if team_county['county'] == 'New York City':
         return county_date['county'] == 'New York City'
     else:
         return county_date['fips'] == team_county['fips']
 
 
-def get_case_inc_avg(team_county):
-    return team_county['case_inc_avg']
-
-
 with open('data/counties.csv', newline='') as counties_csv:
     reader = csv.DictReader(counties_csv)
-    team_counties = []
-    for team_county in reader:
-        if team_county['team']:
-            county_dates = [cd for cd in county_date_data if match(cd, team_county)]
-            last_week_case_diff = int(county_dates[-1]['cases']) - int(county_dates[-7]['cases'])
-            case_inc_avg = 100000 * (last_week_case_diff)/(7*int(team_county['population']))
-            team_county['case_inc_avg'] = case_inc_avg
-            # print(team_county['team'], team_county['county'], round(case_inc_avg, 2))
-            team_counties.append(team_county)
+    county_date_averages = []
 
-    sortedlist = sorted(team_counties, key=get_case_inc_avg, reverse=True)
+    for team_county in reader:
+        county_dates = [cd for cd in county_date_data if match_county(cd, team_county)]
+        county_pop = int(team_county['population'])
+
+        for idx, county_date in enumerate(county_dates):
+            if idx > 6:
+                current_cases = int(county_date['cases'])
+                last_weeks_cases = int(county_dates[idx - 7]['cases'])
+                last_week_case_diff = current_cases - last_weeks_cases
+
+                case_inc_avg = 100000 * (last_week_case_diff)/(7*county_pop)
+                county_date_averages.append({
+                    'county': county_date['county'],
+                    'state': county_date['state'],
+                    'date': county_date['date'],
+                    'case_inc_avg': round(case_inc_avg, 2),
+                    'team': team_county['team'],
+                })
 
     with open('data/case_inc_avg.csv', 'w', newline='\n') as csvfile:
         writer = csv.writer(csvfile, delimiter=',')
-        writer.writerow(['Team', 'County', 'Population', 'New Case Daily Avg/100K'])
-        for tc in sortedlist:
-            writer.writerow([tc['team'], tc['county'], tc['population'], get_case_inc_avg(tc)])
+        writer.writerow(['Team', 'County', 'State', 'Date', 'New Case Daily Avg/100K'])
+        for cda in county_date_averages:
+            writer.writerow([cda['team'], cda['county'], cda['state'], cda['date'], cda['case_inc_avg']])
