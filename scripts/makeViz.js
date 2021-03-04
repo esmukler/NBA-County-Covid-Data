08967298e4ee39aa20813d57733cb8c52705be24
Eli Smukler <esmukler@pandora.com>
2021-03-03 18:18:38 -0800

    Show both bar and line graphs

--- scripts/makeViz.js
+++ scripts/makeViz.js
@@ -1,10 +1,10 @@
 // set the dimensions and margins of the graph
 var margin = {top: 10, right: 30, bottom: 40, left: 60},
-    width = 460 - margin.left - margin.right,
+    width = 900 - margin.left - margin.right,
     height = 700 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
-var svg = d3.select("#my_dataviz")
+var svg = d3.select("#lines-graph")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -12,17 +12,16 @@ var svg = d3.select("#my_dataviz")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
+const massageTheData = (d) => ({
+  date: d3.timeParse("%Y-%m-%d")(d['Date']),
+  caseIncAvg: parseFloat(d['New Case Daily Avg/100K']),
+  team: d['Team'],
+});
+
 //Read the data
-d3.csv("data/case_inc_avg.csv",
+d3.csv("data/case_inc_avg.csv", massageTheData,
 
-  // massage the data
-  (d) => ({
-    date: d3.timeParse("%Y-%m-%d")(d['Date']),
-    caseIncAvg: parseFloat(d['New Case Daily Avg/100K']),
-    team: d['Team'],
-  }),
-
-  // Now I can use this dataset:
+// Now I can use this dataset:
 function(data) {
   var sumstat = d3.nest() // nest function allows to group the calculation per level of a factor
     .key(d => d.team)
@@ -70,4 +69,63 @@ function(data) {
             .y(function(d) { return y(d.caseIncAvg); })
             (d.values)
         })
+})
+
+// append the svg object to the body of the page
+var svgBar = d3.select("#bar-graph")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+// Parse the Data
+d3.csv("./data/case_inc_avg.csv", massageTheData,
+
+function(err, data) {
+  if (err) throw(err);
+
+  data = data.filter(d => {
+    var cutoffDate = new Date();
+    cutoffDate.setDate(cutoffDate.getDate() - 2);
+    return d.date > cutoffDate;
+  });
+
+  data.sort(function(a, b) {
+    return b.caseIncAvg - a.caseIncAvg;
+  });
+  console.log('after sort', data.map(d => d.team));
+
+  // X axis
+  var x = d3.scaleBand()
+    .range([ 0, width ])
+    .domain(data.map(d => d.team))
+    .padding(0.2);
+  svgBar.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+      .attr("transform", "translate(-10,0)rotate(-45)")
+      .style("text-anchor", "end");
+
+  const maxCaseAvg = d3.max(data.map(d => d.caseIncAvg));
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain([0, maxCaseAvg * 1.10])
+    .range([ height, 0]);
+  svgBar.append("g")
+    .call(d3.axisLeft(y));
+
+  // Bars
+  svgBar.selectAll("mybar")
+    .data(data)
+    .enter()
+    .append("rect")
+      .attr("x", function(d) { return x(d.team); })
+      .attr("y", function(d) { return y(d.caseIncAvg); })
+      .attr("width", x.bandwidth())
+      .attr("height", function(d) { return height - y(d.caseIncAvg); })
+      .attr("fill", "orange")
+
 })
